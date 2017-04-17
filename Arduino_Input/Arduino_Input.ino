eda089511a1a6216327bea21eaacc7c917123d63
Aditya Ramani <adityaramani@hotmail.com>
2017-04-17 09:25:18 +0530

    All appliances have own straem

--- Arduino_Input/Arduino_Input.ino
+++ Arduino_Input/Arduino_Input.ino
@@ -1,15 +1,17 @@
 #include <Servo.h>
+
 int pos;
 int DoorInput = A0;
 int RoomInput = A1;
 int ldrInput = A2;
 int TempInput = A3;
 int GasInput = A4;
-int a[5] = {0,0,0,0,0};
-int server[5] = {-1,-1,-1,-1,-1};
-int prev[5] = {0,0,0,0,0};
-
-
+int a[5] = {
+  0,0,0,0,0};
+int server[5] = {
+  -1,-1,-1,-1,-1};
+int prev[5] = {
+  0,0,0,0,0};
 int LightHallEnable = 3;
 int LightBedroomEnable = 4;
 int LightBathroomEnable = 7;
@@ -21,25 +23,33 @@ int Flagservo =1 ;
 int FlagLightHall = 1;
 
 
+/*
+Server [0] = Main Doors
+ Server [1] = hall light
+ Server [2] = bedroom light
+ Server [3] = hall fan
+ Server [4] = bedroom fan
+ 
+ */
 void doors_open()
 {
-   for (pos = 0; pos <= 180; pos += 1) {
-                myservoLeft.write(pos);
-                 myservoRight.write(180 - pos);
-                delay(15);
-       }
-    delay(1000);
+  for (pos = 0; pos <= 180; pos += 1) {
+    myservoLeft.write(pos);
+    myservoRight.write(180 - pos);
+    delay(15);
+  }
+  delay(1000);
 
 }
 
 void doors_close()
 {
 
-      for (pos = 180; pos >= 0; pos -= 1) {
-                myservoLeft.write(pos);
-                myservoRight.write(180 - pos);
-                delay(15);
-      }
+  for (pos = 180; pos >= 0; pos -= 1) {
+    myservoLeft.write(pos);
+    myservoRight.write(180 - pos);
+    delay(15);
+  }
   delay(1000);
 
 
@@ -64,122 +74,187 @@ void setup() {
 int i;
 void loop() {
 
+  for( i = 0; i < 5; i++)
+    prev[i] = server[i];
 
-    if( Serial.available() > 4){
+  if( Serial.available() > 4){
 
-      for( i = 0 ; i < 5; i++){
-            server[i] = Serial.read() - '0';
+    for( i = 0 ; i < 5; i++){
+      server[i] = Serial.read() - '0';
 
-            delay(100);
-      }
-delay(2000);
-   Flagservo = a[0];
+      delay(100);
+    }
+    delay(5000);
 
-}
 
- else
-{
-  a[0] = digitalRead(DoorInput);
-  Flagservo  =a[0];
-  //a[1] is value of IR sensor in room
-  a[1] =  a[0];
 
-  int val = analogRead(ldrInput);
-  if( map(val , 0 , 1023, 0 ,255) > 100)
-      a[2] = 1; //set high cause the surrounding is dark
+    Flagservo = server[0];
+
+  }
+
   else
+  {
+    a[0] = digitalRead(DoorInput);
+    Flagservo  =a[0];
+    //a[1] is value of IR sensor in room
+    a[1]= digitalRead(RoomInput);
+
+    int val = analogRead(ldrInput);
+    if( map(val , 0 , 1023, 0 ,255) > 100)
+      a[2] = 1; //set high cause the surrounding is dark
+    else
       a[2] = 0;
 
-  val = analogRead(TempInput);
-  
-
-  float mv = ( val/1024.0)*5000;
-  float cel = mv/10;
-  //Serial.print("Temp :" );
-  //Serial.println(cel);
-  if(cel > 95.0 ) // if temp is > 27 set a[3] as high , so as to turn on fans..  Change value during presenting
-    a[3] = 1;
-  else
-    a[3] = 0;
-
-   a[4] = digitalRead(GasInput);
-}
-
-if(server[0] == 1){
-  doors_open();
-  delay(1000);
-  doors_close();
-  server[0] = -1;
-}
-
-else if(Flagservo)
- {
-       doors_open();
-       delay(1000);
-       doors_close();
-       Flagservo = 0 ;
-       a[0] = 0;
-}
-
-if(server[2] == 1)
-{
-  digitalWrite(LightHallEnable , HIGH);
-  digitalWrite(LightBedroomEnable , HIGH);
-}
-else if(server[2] == 0 ) {
-  digitalWrite(LightHallEnable , LOW);
-  digitalWrite(LightBedroomEnable ,LOW);
+    val = analogRead(TempInput);
 
 
-}
+    float mv = ( val/1024.0)*5000;
+    float cel = mv/10;
+    if(cel > 20.0 ) // if temp is > 27 set a[3] as high , so as to turn on fans..  Change value during presenting
+      a[3] = 1;
+    else
+      a[3] = 0;
 
-
-else{
-  if(a[2])
- {
-   digitalWrite(LightHallEnable , HIGH);
-   digitalWrite(LightBedroomEnable , HIGH);
+    a[4] = digitalRead(GasInput);
   }
-else{
-  digitalWrite(LightHallEnable , LOW);
-  digitalWrite(LightBedroomEnable ,LOW);
 
-}
-}
+  if(server[0] == 1){
+    doors_open();
+    delay(1000);
+    doors_close();
+    server[0] = -1;
+  }
 
-if(server[3] == 1){
-  digitalWrite(FanHall , HIGH);
-  digitalWrite(FanBedroom , LOW);
+  else if(Flagservo)
+  {
+    doors_open();
+    delay(1000);
+    doors_close();
+    Flagservo = 0 ;
+    a[0] = 0;
+  }
 
-}
-else if(server[3] == 0 )
-{
-    digitalWrite(FanHall , LOW);
-  digitalWrite(FanBedroom , LOW);
+  if(a[1] ==1 ){
 
-}
-else{
-if(a[3])
-{
-  digitalWrite(FanHall , HIGH);
-  digitalWrite(FanBedroom , HIGH);
+    if(a[2] == 1  )
+    {
+      digitalWrite(LightBedroomEnable ,LOW);
+    }
+    if(a[3] == 1)
+    {
+      digitalWrite(FanBedroom , LOW);  
+    }   
+  }
 
- }
-else
-{
-  digitalWrite(FanHall , LOW);
-  digitalWrite(FanBedroom , LOW);
+  else if(a[1] == 0){
 
-}
-}
+    digitalWrite(FanBedroom , LOW);
+    digitalWrite(LightBedroomEnable ,LOW);
+  }
 
 
 
-for(i = 0 ;i < 5 ;i++){
-      Serial.println(a[i]);
-      delay(250);
+  if(server[1] == 1)
+  {
+    digitalWrite(LightHallEnable , HIGH);
+
+  }
+  else if(server[1] == 0 ) {
+    digitalWrite(LightHallEnable , LOW);
+
+
+  }
+
+
+  else{
+    if(a[2])
+    {
+      digitalWrite(LightHallEnable , HIGH);
     }
 
-delay(2000);
+    else{
+      digitalWrite(LightHallEnable , LOW);
+
+    }
+  }
+  //
+
+  if(server[2] == 1)
+  {
+    digitalWrite(LightBedroomEnable , HIGH);
+  }
+  else if(server[2] == 0 ) {
+    digitalWrite(LightBedroomEnable , LOW);
+
+
+  }
+
+
+  else{
+    if(a[2] && a[1] )
+    {
+      digitalWrite(LightBedroomEnable , HIGH);
+    }
+
+    else{
+      digitalWrite(LightBedroomEnable ,LOW);
+
+    }
+  }
+
+  if(server[3] == 1){
+    digitalWrite(FanHall , HIGH);
+
+  }
+  else if(server[3] == 0 )
+  {
+    digitalWrite(FanHall , LOW);
+
+  }
+  else{
+    if(a[3])
+    {
+      digitalWrite(FanHall , HIGH);
+
+    }
+    else
+    {
+      digitalWrite(FanHall , LOW);
+
+    }
+  }
+
+  if(server[4] == 1){
+    digitalWrite(FanBedroom , HIGH);
+
+  }
+  else if(server[4] == 0 )
+  {
+    digitalWrite(FanBedroom , LOW);
+
+  }
+  else{
+    if(a[3] && a[1])
+    {
+      digitalWrite(FanBedroom , HIGH);
+
+    }
+    else
+    {
+
+      digitalWrite(FanBedroom , LOW);
+
+    }
+  }
+
+
+
+  for(i = 0 ;i < 5 ;i++){
+    Serial.println(a[i]);
+    delay(250);
+  }
+
+  delay(1000);
 
 }
+
--- server.py
+++ server.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template, request, url_for
-import threading
 import serial
 import sys
 import time
 
-ard =  serial.Serial('/dev/ttyACM0', 9600)
-outputs= [0 for i in range(5) ]
+ard =  serial.Serial('/dev/ttyACM2', 9600)
+outputs= [0 for i in range(5)]
 
 # Initialize the Flask application
 app = Flask(__name__)
@@ -13,7 +12,6 @@ app = Flask(__name__)
 # Define a route for the default URL, which loads the form
 @app.route('/door', methods=['POST'])
 def door():
-    outputs= [0 for i in range(5) ]
 
     outputs[0] = int(request.form.get('data'))
     for i in range(5):
@@ -22,10 +20,8 @@ def door():
     return "door = " + str(outputs[0])
 
 
-@app.route('/light', methods=['POST'])
+@app.route('/light-room', methods=['POST'])
 def light():
-    outputs= [0 for i in range(5) ]
-
     outputs[2] = int(request.form.get('data'))
 
     for i in range(5):
@@ -37,8 +33,6 @@ def light():
 
 @app.route('/fan', methods=['POST'])
 def fan():
-    outputs= [0 for i in range(5) ]
-
     outputs[3] = int(request.form.get('data'))
     for i in range(5):
         sys.stderr.write(str(outputs[i]))
@@ -46,16 +40,42 @@ def fan():
     return "fan = " + str(outputs[3])
 
 @app.route('/pool', methods=['POST'])
+
 def pool():
-    outputs= [0 for i in range(5) ]
+    outputs= [0 for i in range(5)]
     for i in range(5):
         sys.stderr.write(str(outputs[i]))
         ard.write(str(outputs[i]).encode('utf-8'))
     return "All off"
 
 
+@app.route('/light', methods=['POST'])
+def light_room():
+    outputs[1] = int(request.form.get('data'))
+    for i in range(5):
+        sys.stderr.write(str(outputs[i]))
+        ard.write(str(outputs[i]).encode('utf-8'))
+    return "light room" + str(request.form.get('data'))
+
+
+@app.route('/fan-room', methods=['POST'])
+def fan_room():
+    outputs[4] = int(request.form.get('data'))
+    for i in range(5):
+        sys.stderr.write(str(outputs[i]))
+        ard.write(str(outputs[i]).encode('utf-8'))
+    return "light room" + str(request.form.get('data'))
+
+@app.route('/reset', methods=['POST'])
+def reset():
+    outputs = [-1 for i in range(5)]
+    for i in range(5):
+        sys.stderr.write(str(outputs[i]))
+        ard.write(str(outputs[i]).encode('utf-8'))
+    return "reset" 
+
+
 if __name__ == '__main__':
     app.run(
         host = '0.0.0.0'
-
         )
